f75448fceffaffc96ada278d56ff541761046947
Ashish Rajput <ashishrajput50987@gmail.com>
2022-08-09 00:08:06 +0530

    console message changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.use('/', (req, res) => {
-    res.send('Hello Node with AWS');
+    res.send('Hello Node with AWS Yes I am win for Elastic Beantalks');
 
 });
 app.listen(process.env.PORT || 5000, () => { console.log("Node-AWS running om 5000 post") });
